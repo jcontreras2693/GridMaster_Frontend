cfffb7feb341259fba19631a37c93743aa90af37
Juan Contreras <judacobe2693@gmail.com>
2024-12-11 22:58:33 -0500

    Duck https

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -1,6 +1,6 @@
 api=(function(){
 
-    let linkAzure = "http://gridmaster.duckdns.org/"
+    let linkAzure = "https://gridmaster.duckdns.org/"
     //let linkAzure = "https://74.179.216.35/"
     //let linkAzure = "http://4.156.250.133/"
     //let linkAzure = "https://gridmasterbackend-cdezamajdeadcchu.eastus-01.azurewebsites.net/"
